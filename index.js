f1f4c30dbe1711abb0e4ff926ea7288535bbbf93
Arvin <ACRCRIS@chinabank.ph>
2025-06-04 17:39:39 +0800

    jwt authentication

--- index.js
+++ index.js
@@ -40,6 +40,31 @@ app.post("/api/login", (req, res) => {
     }
 });
 
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, "mySecretKey", (error, user) => {
+            if (error) {
+                return res.status(403).json("Token is not valid!")
+            }
+
+            req.user = user;
+            next();
+        });
+        res.status(401).json("You are not authenticated");
+    }
+}
+
+app.delete("/api/users/:userId", verify, (req, res) => {
+    if (req.user.id === req.params.userId || req.user.isAdmin) {
+        res.status(200).json("User has been deleted.")
+    } else {
+        res.status(403).json("You are not allowed to delete this user!")
+    }
+})
+
 app.listen(5000, () => {
     console.log("Backend Server is running!~");
 })
